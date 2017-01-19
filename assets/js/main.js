5885c9ed5d8179be6ec63407cb0a78735b1f1d75
Emran <emranbd77@gmail.com>
2017-01-19 14:25:43 +0600

    Tesimonial Area

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 $('.element').parallax();
+
+console.log("Kichu Nai, ");
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
